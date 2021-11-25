3de81f71dd346f8dbb5759024b6176fc9b4c5030
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-11-25 23:44:51 +0200

    created eslint

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": "airbnb-base/legacy",
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "rules": {
+      "radix": 0,
+      "no-console": 0,
+      "linebreak-style": 0
+    }
+};
