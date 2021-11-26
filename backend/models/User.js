948ec76614e7c8fc1d4ee1aa8fd36ebb44707a0b
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-11-26 14:15:27 +0200

    added user model

---
+++ backend/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+require('mongoose-type-email');
+mongoose.SchemaTypes.Email.defaults.message = 'Email address is invalid!';
+
+const Users = new mongoose.Schema({
+  Name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: true
+  },
+  Email: {
+    type: mongoose.SchemaTypes.Email,
+    correctTld: true,
+    required: [true, 'Email is required'],
+    unique: true
+  },
+  Password: {
+    type: String,
+    required: [true, 'Password is required'],
+    validate: {
+      validator: (v) => {
+        return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid password!`
+    },
+    LikedShops: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Shops'
+    }]
+  }
+});
+
+Users.plugin(uniqueValidator);
+export default mongoose.model('Users', Users, 'Users');
