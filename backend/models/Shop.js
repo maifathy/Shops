95a40bd4f20fe034f6ffaed2de0da4c81f488849
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-11-26 14:15:09 +0200

    added shop model

---
+++ backend/models/Shop.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+
+const Shops = new mongoose.Schema({
+  Name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: true
+  },
+  LikedByUsers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Users'
+  }]
+});
+
+Shops.plugin(uniqueValidator);
+export default mongoose.model('Shops', Shops, 'Shops');
