38061deede209c8b27311578d0674da593ec1c77
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-11-27 21:14:09 +0200

    added check on id before do search

--- backend/ctrls/ctrlUser.js
+++ backend/ctrls/ctrlUser.js
@@ -2,11 +2,16 @@ import Users from './../models/User.js';
 
 const ctrlUser = {
   getUser: async (req, res, next) => {
-    const response = await Users.findById(req.params.id)
+    if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+      console.log('[GET USER]: Invalid id');
+      res.sendStatus(400);
+      return;
+    }
+    await Users.findById(req.params.id)
       .exec((err, User) => {
         if (err) {
           console.log('err: ', err);
-          res.send(err);
+          res.send(err.message);
         } else if (!User) {
           console.log('not user');
           res.sendStatus(404);
@@ -16,14 +21,18 @@ const ctrlUser = {
         }
         next();
       });
-    return response;
   },
   getUserLikedShops: async (req, res, next) => {
-    const response = await Users.findById(req.params.id)
+    if (!req.params.id.match(/^[0-9a-fA-F]{24}$/)) {
+      console.log('[GET USER]: Invalid id');
+      res.sendStatus(400);
+      return;
+    }
+    await Users.findById(req.params.id)
       .populate({ path: 'Shop', select: '_id Name' })
       .exec((err, User) => {
         if (err) {
-          res.send(err);
+          res.send(err.message);
         } else if (!User) {
           res.sendStatus(404);
         } else {
@@ -31,12 +40,11 @@ const ctrlUser = {
         }
         next();
       });
-    return response;
   },
-  addUser: (req, res, next) => {
-    return (new Users(req.body)).save((err, newUser) => {
+  addUser: async (req, res, next) => {
+    await (new Users(req.body)).save((err, newUser) => {
       if (err) {
-        res.send(err);
+        res.send(err.message);
       } else if (!newUser) {
         res.sendStatus(404);
       } else {
