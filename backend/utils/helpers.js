59effeac5d6c35d8edb213dc34193252e2ce99a3
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-11-28 18:45:46 +0200

    added Location and Point

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,12 +1,16 @@
 import Users from './../models/User.js';
 import Shops from './../models/Shop.js';
 
-export const insertRow = async (id, name, collection) => {
+let loc = { type: 'Point', coordinates: [] };
+
+export const insertRow = async (id, name, collection, lon, lat) => {
   let insertRes;
   if (collection === 'Shop') {
+    loc.coordinates = [lon, lat];
     insertRes = await new Shops(
       {
-        Name: name
+        Name: name,
+        Location: loc
       }
     )
       .save((err) => { if (err) { console.log('error adding shop: ', err.message); } });
@@ -15,7 +19,8 @@ export const insertRow = async (id, name, collection) => {
       {
         Name: name,
         Password: `${name}@A123`,
-        Email: `${name}@gmail.com`
+        Email: `${name}@gmail.com`,
+        Location: loc
       }
     )
       .save((err) => { if (err) { console.log('error adding user: ', err.message); } });
