356c687291a9a643f27e0202f5e9bfc2e9c9f5ea
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-11-30 10:49:40 +0200

    added shop fetch requests

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,27 @@
+import { getData, postData, putData, deleteData, api } from './helpers.js';
+
+// shop requests
+export function getNearShops(userId, page){
+  return getData(`${api}/Shops/Near/${userId}/${page}`)
+    .then(shops => ({ shops }));
+}
+
+export function getLikedShops(userId, page){
+  return getData(`${api}/Shops/Like${userId}/${page}`)
+    .then(shops => ({ shops }));
+}
+
+export function likeShop(shopId, userId){
+  return putData(`${api}/Shop/Like/${shopId}?user_id=${userId}`)
+    .then(shop => { shop });
+}
+
+export function dislikeShop(shopId, userId){
+  return putData(`${api}/Shop/Dislike/${shopId}?user_id=${userId}`)
+    .then(shop => { shop });
+}
+
+export function deleteShop(shopId){
+  return deleteData(`${api}/Shop/Remove/${shopId}`)
+  .then(shopId);
+}
