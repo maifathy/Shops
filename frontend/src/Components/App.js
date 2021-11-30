0dbc248eada85e3ffaf269b3114702d4998c8697
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-11-30 13:09:57 +0200

    building up

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Shop shop={{name: 'Mai', isLiked: false}} />
+        <Shop shop={{name: 'Mai', isLiked: false, shopId: '61a47f7f3e317fa6634a7de0'}} userId='61a39ce307772a185e4a2024' />
       </div>
     );
   }
