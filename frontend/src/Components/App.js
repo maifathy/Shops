a78315a914d41edb11a5133394c7aeb28f4666ca
Mai Fathy <66532989+maifathy@users.noreply.github.com>
2021-12-02 11:42:18 +0200

    added geolocation, routes

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,15 +1,59 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+//import LoadingBar from 'react-redux-loading';
+import { Router, Route, Navigate, Routes } from 'react-router-dom';
+import Login from './Login.js';
+import Shops from './Shops.js';
+import NavMenu from './NavMenu.js';
+import history from '../utils/history.js';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import './../App.css';
-import Shop from './Shop.js';
 
-class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <Shop shop={{name: 'Mai', isLiked: false, shopId: '61a47f7f3e317fa6634a7de0'}} userId='61a39ce307772a185e4a2024' />
-      </div>
+      <Router history={history} location={history.location} navigator={history}>
+        <Fragment>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            {this.props.loading === true
+              ? <Navigate
+                  to={{
+                    pathname: '/login',
+                    search: '',
+                    state: { referrer: window.location.pathname }
+                  }}
+                />
+              :
+              <>
+                <Route path='/' element={NavMenu} />
+                <Route path='/Shops' exact element={
+                  <Shops isPreferedPage={false} />
+                } />
+                <Route path='/Shops/Prefered' exact element={
+                  <Shops isPreferedPage={true} />
+                } />
+              </>
+            }
+            </Routes>
+          </Fragment>
+      </Router>
     );
   }
 }
+App.propTypes = {
+  loading: PropTypes.bool
+};
 
 export default App;
